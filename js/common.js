3bfad3d522966207e6cd363913ca77e7cb8fba3f
Vadim Ilchenko <vvadimko@gmail.com>
2017-08-12 11:07:48 +0300

    fixed mobile devices landscape mode

--- js/common.js
+++ js/common.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
 
-if ($(document).width() > 767) {
-	if(!!('ontouchstart' in window)){//check for touch device
-		$(".image--touch .image__overlay-content").css("display","none");
-		$(".image--touch").click(function() {
+	if ($(document).width() > 767) {
+		if(!!('ontouchstart' in window)){//check for touch device
 			$(".image--touch .image__overlay-content").css("display","none");
-			$(this).children(".image__overlay-content").fadeIn();
-		});
+			$(".image--touch").click(function() {
+				$(".image--touch .image__overlay-content").css("display","none");
+				$(this).children(".image__overlay-content").fadeIn();
+			});
+		};
 	};
-};
-
-	
-
 
 	$(".spincrement").css("visibility","hidden")
 	$(".spincrement--main").css("visibility","hidden")
@@ -63,28 +60,28 @@ if ($(document).width() > 767) {
     time   = 5000,
     bar = $('.hd-slider__progress-bar');
 
-function showPane(index){// generic showPane
-    // hide current pane
-    ePanes.eq(currentIndex).stop(true, true).fadeOut();
-    // set current index : check in panes collection length
-    currentIndex = index;
-    if(currentIndex < 0) currentIndex = ePanes.length-1;
-    else if(currentIndex >= ePanes.length) currentIndex = 0;
-    // display pane
-    ePanes.eq(currentIndex).stop(true, true).fadeIn();
-    // menu selection
-    $('.hd-slider-nav__item').removeClass('current').eq(currentIndex).addClass('current');
-    $('.progress-bar__item').removeClass('current').eq(currentIndex).addClass('current');
-}
+	function showPane(index){// generic showPane
+	    // hide current pane
+	    ePanes.eq(currentIndex).stop(true, true).fadeOut();
+	    // set current index : check in panes collection length
+	    currentIndex = index;
+	    if(currentIndex < 0) currentIndex = ePanes.length-1;
+	    else if(currentIndex >= ePanes.length) currentIndex = 0;
+	    // display pane
+	    ePanes.eq(currentIndex).stop(true, true).fadeIn();
+	    // menu selection
+	    $('.hd-slider-nav__item').removeClass('current').eq(currentIndex).addClass('current');
+	    $('.progress-bar__item').removeClass('current').eq(currentIndex).addClass('current');
+	}
 
-showPane(0);
+	showPane(0);
 
-function run(){
-    showPane(currentIndex+1);
-    var item = $('.progress-bar__item-filler').eq(currentIndex);
-    item.width(0);
-    item.animate({'width': "+=100%"}, time, run);
-}
+	function run(){
+	    showPane(currentIndex+1);
+	    var item = $('.progress-bar__item-filler').eq(currentIndex);
+	    item.width(0);
+	    item.animate({'width': "+=100%"}, time, run);
+	}
 
 run();
 
@@ -112,11 +109,6 @@ $(".owl-carousel").owlCarousel({
 	});
 
 	$('.popup_c').magnificPopup();
-
-	$.stellar({
-		responsive: true,
-
-	});
 
 
 });
